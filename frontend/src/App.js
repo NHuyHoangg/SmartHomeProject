3c5eea47bd03fdb11d97a533164d4f2a34b3b760
Nhat Ha <ng.nhatha32@gmail.com>
2023-04-04 02:29:14 +0700

    route

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,23 @@
+import { useState } from "react";
+
 import Index from "./index/index";
 import Warning from "./warning/warning";
-import Login from "./login/login"
-import Signup from "./signup/signup"
-import Profile from "./profile/profile"
-import Notify from "./notify/notify";
+import Login from "./login/login";
+import Signup from "./signup/signup";
 
 function App() {
+
+    const [user, setUser] = useState();
+    const [login, setLogin] = useState(false);
+    const [signup, setSignup] = useState(false);
+    const [openWarning, setOpenWarning] = useState(false);
+
     return (
         <div className="App">
-            <Warning />
-            {/* <Index /> */}
-            {/* <Login /> */}
-            {/* <Signup /> */}
-            {/* <Profile /> */}
-            {/* <Notify /> */}
+            {signup &&  <Signup setUser={setUser} setSignup={setSignup}/>}
+            {!login && !signup && <Login setUser={setUser} setSignup={setSignup} setLogin={setLogin}/>}
+            {login && !openWarning && <Index setOpenWarning={setOpenWarning} setLogin={setLogin}/>}
+            {login && openWarning && <Warning setOpenWarning={setOpenWarning} />}
         </div>
     )
 }
