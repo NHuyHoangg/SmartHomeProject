8f47985a28628f29421bfbaca4a8ec3b8acd59bc
gerphan <germanphan2002@gmail.com>
2023-04-13 17:31:49 +0700

    loading view

---
+++ frontend/src/loading/loading.jsx
@@ -0,0 +1,16 @@
+import Sidebar from '../sidebar/sidebar'
+import './loading.css'
+
+export default function Loading() {
+    return (
+        <div className='Loading'>
+            <Sidebar />
+            <div class="preview">
+                <svg viewBox="0 0 50 50" class="spinner">
+                    <circle class="ring" cx="25" cy="25" r="22.5" />
+                    <circle class="line" cx="25" cy="25" r="22.5" />
+                </svg>
+            </div>
+        </div>
+    )
+}
